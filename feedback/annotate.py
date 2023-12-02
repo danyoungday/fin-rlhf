0a18e3c39dc18fb2076626b35581ac354af188c2
Daniel Young <danyoung@utexas.edu>
2023-12-02 17:11:04 -0500

    Finished labeling

--- feedback/annotate.py
+++ feedback/annotate.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     if not os.path.exists(args.labels_path):
         with open(args.labels_path, "w") as f:
-            f.write("label\n")
+            f.write("label,bad\n")
     
     df = pd.read_csv(args.unlabeled_path)
     n = sum(1 for _ in open(args.labels_path)) - 1
@@ -23,10 +23,15 @@ if __name__ == "__main__":
         for i in range(n, len(df)):
             row = df.iloc[i]
             print("".join(["=" for _ in range(100)]))
-            print(f"Prompt: {repr(row['prompt'])}")
+            print(f"Prompt {i}/{len(df)} ({int(i / len(df) * 100)}%): {repr(row['prompt'])}")
             print("".join(["-" for _ in range(100)]))
             print(f"Response A: {repr(row['response_a'])}")
             print("".join(["-" for _ in range(100)]))
             print(f"Response B: {repr(row['response_b'])}")
-            label = input("1 for prompt A, 2 for prompt B")
-            f.write(f"{int(label)-1}\n")
+            label = int(input("1 for prompt A, 2 for prompt B. If both responses bad 3 for prompt A, 4 for prompt B: "))
+            if label < 3:
+                f.write(f"{label-1},0\n")
+            else:
+                f.write(f"{label-3},1")
+            print("".join(["-" for _ in range(100)]))
+            print(f"Chose {label}")
